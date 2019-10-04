7ce462b75c97ac5682bf5097f5f14098529bda65
bhargavi2804 <bhargavi.nandi@gmail.com>
2019-10-04 17:48:26 +0530

    4/10

---
+++ unittest/implementation.c
@@ -0,0 +1,7 @@
+#include "implementation.h"
+
+int sum(int a, int b) {
+
+    return a + b;
+}
+
---
+++ unittest/main.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int sum(int a, int b);
+void main()
+{
+	int c;
+	
+	c=sum(3,2);
+	printf("sum=%d",c);
+}
